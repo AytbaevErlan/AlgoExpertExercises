8aa3d4cda9d71b2eca45a8987d5d49a9843b3dcf
hyper <hyperskill@hyperskill.org>
2025-05-12 15:26:27 +0200

    easy - coding challenge | AlgoExpert

---
+++ codingChalange/easy/SortedSquaredArray.java
@@ -0,0 +1,18 @@
+package codingChalange.easy;
+
+import java.util.*;
+
+class SortedSquaredArray {
+    public int[] sortedSquaredArray(int[] array) {
+        int[] ans = new int[array.length];
+
+        for(int i = 0; i < array.length; i++){
+            ans[i] = array[i] * array[i];
+        }
+
+        Arrays.sort(ans);
+
+        return ans;
+    }
+}
+
